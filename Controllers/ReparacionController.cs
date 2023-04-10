42f6be4c8fc19434be675c01a2d5dbce0544b471
marlontunchez <48894768+marlontunchez@users.noreply.github.com>
2023-04-09 19:24:45 -0600

    modal de reparacion funcionando

--- Controllers/ReparacionController.cs
+++ Controllers/ReparacionController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SPCOIN.Models;
 using System.Data.SqlClient;
 
@@ -40,7 +41,6 @@ namespace SPCOIN.Controllers
                                     Motocicleta = Convert.ToString(reader["MOTOCICLETA"]),
                                     Mecanico = Convert.ToString(reader["MECANICO"]),
                                     ConvertidoAVenta = Convert.ToBoolean(reader["CONVERTIDOAVENTA"])
-
                                 };
                                 reparacionesrealizadas.Add(reparacion);
                             }
@@ -58,56 +58,10 @@ namespace SPCOIN.Controllers
         }
 
 
-      
+        [AllowAnonymous]
         public IActionResult Create()
         {
-            //try
-            //{
-            //    using (SqlConnection con = new(_context.Conexion))
-            //    {
-            //        using (SqlCommand cmd = new("SP_LOGIN", con))
-            //        {
-            //            cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            //            cmd.Parameters.Add("@USUARIO", System.Data.SqlDbType.VarChar).Value = u.User;
-            //            cmd.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarChar).Value = u.Contraseña;
-            //            SqlParameter codigousuarioParam = cmd.Parameters.Add("@CODIGOUSUARIO", System.Data.SqlDbType.Int); // Agregar parámetro de salida
-            //            codigousuarioParam.Direction = System.Data.ParameterDirection.Output; // Indicar que es un parámetro de salida
-            //            con.Open();
-            //            cmd.ExecuteNonQuery(); // Ejecutar el comando                        
-            //            int codigousuario = codigousuarioParam.Value == DBNull.Value ? 0 : (int)codigousuarioParam.Value; // Validar si es nulo y asignar cero en su lugar
-            //            TempData["codigousuario"] = codigousuario;
-            //            if (Convert.ToUInt32(TempData["codigousuario"].ToString()) > 0)
-            //            {
-            //                List<Claim> c = new List<Claim>();
-            //                {
-            //                    new Claim(ClaimTypes.NameIdentifier, u.User);
-            //                };
-            //                ClaimsIdentity ci = new(c, CookieAuthenticationDefaults.AuthenticationScheme);
-            //                AuthenticationProperties p = new();
-            //                p.AllowRefresh = true;
-
-            //                p.ExpiresUtc = DateTime.UtcNow.AddHours(1);
-            //                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(ci), p);
-            //                return RedirectToAction("Index", "Sucursal");
-
-            //            }
-            //            else
-            //            {
-            //                ViewBag.Error = "Verifique sus credenciales de acceso";
-            //            }
-            //        }
-            //        con.Close();
-
-
-            //    }
-            //    return View();
-
-            //}
-            //catch (System.Exception e)
-            //{
-            //    ViewBag.Error = e.Message;
-            //    return View();
-            //}
+            
             return View();
         }
        
