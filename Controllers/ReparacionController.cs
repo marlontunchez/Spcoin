ee6e181128960fcc65d1f0e3c6a8fb73350f593a
marlontunchez <48894768+marlontunchez@users.noreply.github.com>
2023-04-11 22:57:46 -0600

    reparaciones funcionando

--- Controllers/ReparacionController.cs
+++ Controllers/ReparacionController.cs
@@ -58,8 +58,8 @@ namespace SPCOIN.Controllers
         }
 
 
-        [AllowAnonymous]
-                public async Task<IActionResult> Create(Reparacion u)
+        [HttpPost]
+                public async Task<IActionResult> Create(Reparacion r)
         {
             try
             {
@@ -71,8 +71,8 @@ namespace SPCOIN.Controllers
                         cmd.Parameters.Add("@CODIGOCLIENTE", System.Data.SqlDbType.BigInt).Value = 1;
                         cmd.Parameters.Add("@CODIGOASIGNACIONPERMISOS", System.Data.SqlDbType.BigInt).Value = 1;
 
-                        cmd.Parameters.Add("@MOTOCICLETA", System.Data.SqlDbType.VarChar).Value = u.Motocicleta ;
-                        cmd.Parameters.Add("@MECANICO", System.Data.SqlDbType.VarChar).Value = u.Mecanico;
+                        cmd.Parameters.Add("@MOTOCICLETA", System.Data.SqlDbType.VarChar).Value = r.Motocicleta ;
+                        cmd.Parameters.Add("@MECANICO", System.Data.SqlDbType.VarChar).Value = r.Mecanico;
                         con.Open();
                         cmd.ExecuteNonQuery(); // Ejecutar el comando                        
                     }
@@ -91,6 +91,39 @@ namespace SPCOIN.Controllers
         }
 
 
+
+       
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int codigoReparacion)
+        {
+            try
+            {
+                // Validar que el código de reparación es un número válido
+                if (!int.TryParse(codigoReparacion.ToString(), out _))
+                {
+                    return Json(new { success = false, message = "El código de reparación no es válido" });
+                }
+
+                using (SqlConnection con = new(_context.Conexion))
+                {
+                    using (SqlCommand cmd = new("DREPARACION", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@CODIGOREPARACION", System.Data.SqlDbType.BigInt).Value = codigoReparacion;
+                        con.Open();
+                        cmd.ExecuteNonQuery(); // Ejecutar el comando                        
+                    }
+                    con.Close();
+                }
+                return Json(new { success = true }); ;
+
+            }
+            catch (System.Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return Json(new { success = false, message = e.Message });
+            }
+        }
 
 
 
