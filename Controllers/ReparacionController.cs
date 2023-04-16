725245cc4d377f2bd64ef99337193683ca36c6cd
marlontunchez <48894768+marlontunchez@users.noreply.github.com>
2023-04-16 08:56:11 -0600

    eliminacion detalle funcionando todo al 100

--- Controllers/ReparacionController.cs
+++ Controllers/ReparacionController.cs
@@ -245,5 +245,38 @@ namespace SPCOIN.Controllers
         }
 
 
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDetail(int codigoDetalleReparacion)
+        {
+            try
+            {
+                // Validar que el código de reparación es un número válido
+                if (!int.TryParse(codigoDetalleReparacion.ToString(), out _))
+                {
+                    return Json(new { success = false, message = "El código de detalle reparación no es válido" });
+                }
+
+                using (SqlConnection con = new(_context.Conexion))
+                {
+                    using (SqlCommand cmd = new("DDETALLEREPARACION", con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@CODIGODETALLEREPARACION", System.Data.SqlDbType.BigInt).Value = codigoDetalleReparacion;
+                        con.Open();
+                        cmd.ExecuteNonQuery(); // Ejecutar el comando                        
+                    }
+                    con.Close();
+                }
+                return Json(new { success = true }); ;
+
+            }
+            catch (System.Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return Json(new { success = false, message = e.Message });
+            }
+        }
+
     }
 }
