eb71effca324584e9b50a4a3e613e9944078f361
marlontunchez <48894768+marlontunchez@users.noreply.github.com>
2023-05-03 06:54:01 -0600

    cliente models

---
+++ Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace SPCOIN.Properties
+{
+    public class Cliente
+    {
+        public int CodigoCliente { get; set; }
+        public string? Nombre { get; set; }
+        public string? Empresa { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Nit { get; set; }
+        public int DiasDeCredito { get; set; }
+        public int LimiteCredito { get; set; }
+        public string? TipoCliente { get; set; }
+        public int PorcentajeDescuento { get; set; }
+        public int PorcentajeAumento { get; set; }
+        public string? Correo { get; set; }
+    }
+}
