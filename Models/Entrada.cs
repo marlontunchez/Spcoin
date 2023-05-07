ed0a3691950513982eef66eb246114bff2315a9f
marlontunchez <48894768+marlontunchez@users.noreply.github.com>
2023-05-07 16:20:52 -0600

    entrada iniciando

---
+++ Models/Entrada.cs
@@ -0,0 +1,18 @@
+﻿namespace SPCOIN.Models
+{
+    public class Entrada
+    {
+        public int CodigoEntrada { get; set; }
+        public int Correlativo { get; set; }
+        public DateTime Fecha { get; set; }
+        public string? Sucursal { get; set; }
+        public string? Descripcion { get; set; }
+        public decimal Total { get; set; }
+        public string? Estado { get; set; }
+
+
+
+
+
+    }
+}
