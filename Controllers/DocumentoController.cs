c3628cfd54207daf175777b7a57760f6856e1adf
marlontunchez <48894768+marlontunchez@users.noreply.github.com>
2023-12-05 21:06:19 -0600

    con nuevo dashboard

---
+++ Controllers/DocumentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPCOIN.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
